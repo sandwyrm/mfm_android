1614873e6914fb024bca4d874fd46b55a69f6fb9
sandwyrm <sandwyrm@comfreeze.net>
2011-11-19 15:59:07 -0800

    Migrating dashboard activity to new namespace and moving testing logic out to external activity for emote selector.

---
+++ My Face Messenger/src/com/myfacemessenger/android/activity/DashboardActivity.java
@@ -0,0 +1,41 @@
+package com.myfacemessenger.android.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.myfacemessenger.android.R;
+
+public class DashboardActivity extends Activity
+{
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		((Button) findViewById(R.id.getImage))
+			.setOnClickListener(imageSelectionListener);
+	}
+
+	private OnClickListener imageSelectionListener =	//
+		new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			Intent intent = new Intent(getBaseContext(), FaceIconManagerActivity.class);
+			startActivity(intent);
+		}
+	};
+}
