28d9e2833e29aa15781add840431700e68651d9b
James Zimmerman II <james.zimmerman@comfreeze.net>
2011-11-27 23:55:50 -0800

    Extending existing logic to include defined API features.

--- My Face Messenger/src/com/myfacemessenger/android/api/APIHandler.java
+++ My Face Messenger/src/com/myfacemessenger/android/api/APIHandler.java
@@ -8,7 +8,11 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
@@ -40,7 +44,85 @@ public class APIHandler
 		client = new DefaultHttpClient(httpParams);
 	}
 
-	public void register(String number, String email)
+	protected HttpResponse request(HttpUriRequest request)
+	{
+		HttpResponse response = null;
+		try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+
+	protected HttpResponse post(String url, String data)
+	{
+		HttpResponse response = null;
+		HttpPost request = new HttpPost(url);
+		try {
+			StringEntity s = new StringEntity(data);
+			s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+			request.setEntity(s);
+			request.addHeader("Content-Type", "application/json");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		response = request(request);
+		return response;
+	}
+
+	protected HttpResponse put(String url, String data)
+	{
+		HttpResponse response = null;
+		HttpPut request = new HttpPut(url);
+		try {
+			StringEntity s = new StringEntity(data);
+			s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+			request.setEntity(s);
+			request.addHeader("Content-Type", "application/json");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		response = request(request);
+		return response;
+	}
+
+	protected HttpResponse delete(String url)
+	{
+		HttpResponse response = null;
+		HttpDelete request = new HttpDelete(url);
+		request.addHeader("Content-Type", "application/json");
+		response = request(request);
+		return response;
+	}
+
+	protected HttpResponse get(String url)
+	{
+		HttpResponse response = null;
+		HttpGet request = new HttpGet(url);
+		request.addHeader("Content-Type", "application/json");
+		response = request(request);
+		return response;
+	}
+
+	protected JSONObject processResponse(HttpResponse response)
+	{
+		JSONObject responseData = null;
+		try {
+			responseData = new JSONObject(response.getEntity().getContent().toString());
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseData;
+	}
+
+	public void registerUser(String number, String email)
 	{
 		JSONObject requestData = new JSONObject();
 		JSONObject requestUser = new JSONObject();
@@ -54,28 +136,11 @@ public class APIHandler
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		HttpPost request = new HttpPost(USERS_URL);
-		try {
-			HttpEntity entity;
-			StringEntity s = new StringEntity(requestData.toString());
-			s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-			entity = s;
-			request.setEntity(s);
-			request.addHeader("Content-Type", "application/json");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		try {
-			lastResponse = client.execute(request);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		lastResponse = post( USERS_URL, requestData.toString() );
 		MFMessenger.log("Response received: " + lastResponse.toString());
 	}
 
-	public void update(String number, String email)
+	public void updateUser(String number, String email)
 	{
 		JSONObject requestData = new JSONObject();
 		JSONObject requestUser = new JSONObject();
@@ -89,24 +154,42 @@ public class APIHandler
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		HttpPost request = new HttpPost(FACE_URL + "/" + URLEncoder.encode(number));
+		lastResponse = put( FACE_URL + "/" + number, requestData.toString() );
+		MFMessenger.log("Response received: " + lastResponse.toString());
+	}
+
+	public void deleteUser(String number)
+	{
+		lastResponse = delete( FACE_URL + "/" + number );
+	}
+
+	public JSONObject userInformation(String number)
+	{
+		lastResponse = get( FACE_URL + "/" + number );
+		return processResponse(lastResponse);
+	}
+
+	public JSONObject getFace(String number, String emote)
+	{
+		lastResponse = get( FACE_URL + "/" + number + "/faces/" + emote );
+		return processResponse(lastResponse);
+	}
+
+	public void updateFace(String number, String email)
+	{
+		JSONObject requestData = new JSONObject();
+		JSONObject requestUser = new JSONObject();
 		try {
-			HttpEntity entity;
-			StringEntity s = new StringEntity(requestData.toString());
-			s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
-			entity = s;
-			request.setEntity(s);
-			request.addHeader("Content-Type", "application/json");
-		} catch (UnsupportedEncodingException e) {
+			requestUser.put("mobile_number", number);
+			if( email != null ) {
+				requestUser.put("email", email);
+			}
+			requestData.put("user", requestUser);
+			MFMessenger.log("Request to be sent: " + requestData.toString());
+		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		try {
-			lastResponse = client.execute(request);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		lastResponse = put(FACE_URL + "/" + number, requestData.toString());
 		MFMessenger.log("Response received: " + lastResponse.toString());
 	}
 }
