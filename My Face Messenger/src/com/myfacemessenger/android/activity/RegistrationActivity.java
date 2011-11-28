e471a7657bf9b720ad07eae200e8f0af4cd63899
James Zimmerman II <james.zimmerman@comfreeze.net>
2011-11-28 08:02:31 -0800

    Adding registration screen.

---
+++ My Face Messenger/src/com/myfacemessenger/android/activity/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.myfacemessenger.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
